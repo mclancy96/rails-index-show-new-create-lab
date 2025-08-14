58c55ec1db3396ee185a323a965330871d3d0969
Mike <mclancy96@gmail.com>
2025-08-14 12:12:14 -0400

    Build out controller methods

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,5 +2,22 @@ class CouponsController < ApplicationController
   def index
     @coupons = Coupon.all
   end
+
+  def create
+    @coupon = Coupon.create!(coupon_params)
+    redirect_to coupon_path(@coupon)
+  end
+
+  def new; end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  private
+
+  def coupon_params
+    params.permit(:store, :coupon_code)
+  end
 end
 
